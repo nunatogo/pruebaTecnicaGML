68b74962f0fb50a712ab2f877961d64f3899c927
Natalia Toro González <torogonzalez@gmail.com>
2025-04-01 11:56:58 -0500

    Refactor

--- src/main/java/com/pokemon/api/controller/PokemonController.java
+++ src/main/java/com/pokemon/api/controller/PokemonController.java
@@ -1,8 +1,10 @@
 package com.pokemon.api.controller;
 
-import com.pokemon.api.services.PokemonDbService;
 import com.pokemon.api.services.PokemonService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
